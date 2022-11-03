9e532ec37a1d7e4c22379c683c27127be162cf71
Andrew Sansum <sansumandrew@gmail.com>
2022-11-03 15:41:40 +0000

    Add initial fields

--- Assets/Scripts/SwordSwing.cs
+++ Assets/Scripts/SwordSwing.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class SwordSwing : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Camera camera;
+
+    private Collider2D collider;
+
     void Start()
     {
-        
+        collider = GetComponent<Collider2D>();
+        collider.enabled = false;
     }
-
-    // Update is called once per frame
     void Update()
     {
         
