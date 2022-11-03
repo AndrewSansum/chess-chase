fc300fabe1dedda5ec36416c49cb55c4a0312f9b
Andrew Sansum <sansumandrew@gmail.com>
2022-11-03 16:42:28 +0000

    Add sword Transform as field

--- Assets/Scripts/SwordSwing.cs
+++ Assets/Scripts/SwordSwing.cs
@@ -7,21 +7,24 @@ public class SwordSwing : MonoBehaviour
 
     private Collider2D collider;
 
+    private Transform swordTransform;
+
     void Start()
     {
         collider = GetComponent<Collider2D>();
         collider.enabled = false;
+        swordTransform = this.gameObject.transform.parent.transform;
     }
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) {
-            Vector2 playerPosition = this.gameObject.transform.parent.transform.position;
+            Vector2 playerPosition = swordTransform.position;
             Vector2 mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
             float angleFromPlayerToMouse = Vector2.Angle(new Vector2(0,1), mousePosition - playerPosition);
             if (mousePosition.x >= playerPosition.x) {
                 angleFromPlayerToMouse *= -1;
             }
-            this.gameObject.transform.parent.transform.rotation = Quaternion.Euler(0,0,angleFromPlayerToMouse);
+            swordTransform.rotation = Quaternion.Euler(0,0,angleFromPlayerToMouse);
         }
     }
 }
