6064159bcb52434ef24af1df6119adc77926816f
Andrew Sansum <sansumandrew@gmail.com>
2022-11-07 22:52:11 +0000

    Add grid values and initialisation

--- Assets/Scripts/EnemyGrid.cs
+++ Assets/Scripts/EnemyGrid.cs
@@ -8,15 +8,81 @@ public class EnemyGrid : MonoBehaviour
 {
     private Tilemap tilemap;
 
-    // Start is called before the first frame update
-    void Start()
+    private int xOffset;
+    private int yOffset;
+    private int xSize;
+    private int ySize;
+
+    private GridValue[,] grid;
+
+    void Awake()
     {
         tilemap = GetComponent<Tilemap>();
+        BoundsInt tileBounds = tilemap.cellBounds;
+        xOffset = tileBounds.xMin;
+        yOffset = tileBounds.yMin;
+        xSize = tileBounds.xMax - tileBounds.xMin;
+        ySize = tileBounds.yMax - tileBounds.yMin;
+        grid = new GridValue[xSize,ySize];
+        for (int x = 0; x < xSize; x++)
+        {
+            for (int y = 0; y < ySize; y++) 
+            {
+                grid[x, y] = new GridValue();
+                if (!tilemap.HasTile(new Vector3Int(x + xOffset, y + yOffset, 0))) {
+                    grid[x, y].SetObstacle();
+                }
+            }
+        }        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+}
+
+public class GridValue
+{
+    private enum GridStatus {
+        Empty,
+        Obstacle,
+        Reserved
+    }
+
+    private GridStatus status;
+    private int value = 0;
+
+    public GridValue() {
+        status = GridStatus.Empty;
+    }
+
+    public override string ToString() {
+        return status.ToString();
+    }
+
+    public void SetEmpty() {
+        status = GridStatus.Empty;
+    }
+
+    public bool IsEmpty() {
+        return status == GridStatus.Empty;
+    }
+
+    public void SetObstacle() {
+        status = GridStatus.Obstacle;
+    }
+
+    public bool IsObstacle() {
+        return status == GridStatus.Obstacle;
+    }
+
+    public void SetReserved(int value) {
+        status = GridStatus.Reserved;
+        this.value = value;
+    }
+
+    public bool IsReserved() {
+        return status == GridStatus.Reserved;
+    }
+
+    public int GetValue() {
+        return value;
     }
 }
