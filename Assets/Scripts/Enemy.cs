6cc7f9415eae4340c971875250f29a825c2814b7
Andrew Sansum <sansumandrew@gmail.com>
2022-11-08 14:55:59 +0000

    Create main Enemy script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyMovement))]
+public class Enemy : MonoBehaviour
+{
+    public float AttackInterval;
+}
