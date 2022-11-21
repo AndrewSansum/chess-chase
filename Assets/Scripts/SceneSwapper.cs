b8c8eaf5396791b025f51d26a4f4cbcd906b8563
Andrew Sansum <sansumandrew@gmail.com>
2022-11-21 17:43:32 +0000

    Add SceneSwapper script

---
+++ Assets/Scripts/SceneSwapper.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwapper : MonoBehaviour
+{
+    public static void LoadMainMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public static void LoadLevelMenu() {
+        SceneManager.LoadScene("LevelScene");
+    }
+
+    public static void LoadTutorial() {
+        SceneManager.LoadScene("TutorialOne");
+    }
+}
